46de2372dab3401cb47108e10b82b6618fb53302
Jeffry Arrieta <jeffryunad@gmail.com>
2026-05-17 17:05:32 -0500

    fix: ajuste d ela altura del componente slider

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,14 +3,12 @@ function Banner() {
     <section className="banner">
       <div className="banner-text">
         <h1>Tu salud no es a medias, debe ser total</h1>
-        <p>
-          Accede a nuestros servicios de salud de forma rápida y sencilla.
-        </p>
+        <p>Accede a nuestros servicios de salud de forma rápida y sencilla.</p>
         <button>Conocer más</button>
       </div>
 
       <div className="banner-img">
-        <img src="/banner.jpg" alt="Salud Total" />
+        <img src="/banner.jpg" alt="dentvision" />
       </div>
     </section>
   );
